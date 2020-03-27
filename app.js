9040aad30a0f3e8575d095fed68ff496f7f84de0
Urban Vidovič <urbanfoundit@gmail.com>
2020-03-27 09:26:19 +0100

    Added initial mongodb conection

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const MongoDB = require('mongodb');
+const mongoAuthUri = 'mongodb+srv://backend-dev:1234backend@database-fc7vq.mongodb.net/test?retryWrites=true&w=majority'
 
 const router = require('./modules/router');
+// eslint-disable-next-line max-len
+const mongoClient = new MongoDB.MongoClient(mongoAuthUri, {useUnifiedTopology: true});
 
 const app = new Koa();
 const port = 3000;
 
-app.use(bodyParser());
+mongoClient.connect(err => {
+    if(err) throw err; 
+});
 
+app.use(bodyParser());
 app.use(router());
 app.listen(port);
 console.log(`listening on port ${port}`);
