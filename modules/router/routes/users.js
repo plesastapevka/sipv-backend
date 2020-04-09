5f0621cf40f2e7333240e8c09f37091ad555d3e6
aserzhqa54zwsz <51796681+aserzhqa54zwsz@users.noreply.github.com>
2020-04-09 14:09:10 +0200

    funkcija za registracijo

--- modules/router/routes/users.js
+++ modules/router/routes/users.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const router = new Router();
 
+const {Users} = require('../../config/mongo');
 
 /* PRIMER API ENDPOINTA IN KAKO SE NAJ PIŠEJO */
 router.get('/api/getUser/:user', async (ctx) => {
@@ -22,6 +23,44 @@ router.get('/api/getUser/:user', async (ctx) => {
   ctx.status = code;
   ctx.body = {
     data,
+    error,
+    success,
+  };
+});
+
+// Registracija novega uporabnika.
+router.post('/api/registerUser', async (ctx) => {
+  let code = null;
+  let success = null;
+  let error = null;
+  let out = null;
+  let newUser = null;
+  try {
+    const {body} = ctx.request;
+    out = await Users().findOne({email: body.email, username: body.username});
+    if (empty(out)) {
+      newUser = await Users().insertOne({email: body.email, username: body.username, pass: body.pass});
+      if (!empty(newUser)) {
+        code = 200;
+        success = true;
+        error = false;
+      } else {
+        code = 500;
+        success = false;
+        error = 'Error inserting in database!';
+      }
+    } else {
+      code = 500;
+      success = false;
+      error = 'User with same name already exists!';
+    }
+  } catch (err) {
+    success = false;
+    code = 500;
+    error = err;
+  }
+  ctx.status = code;
+  ctx.body = {
     error,
     success,
   };
